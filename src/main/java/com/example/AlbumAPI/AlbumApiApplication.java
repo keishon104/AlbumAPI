88d76ab9155f6ae50e3ba7f491aede1d6792e6da
Keishon <keishon104@gmail.com>
2020-07-28 11:47:25 -0400

    Downloads Java Folder through Spring.io

---
+++ src/main/java/com/example/AlbumAPI/AlbumApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.AlbumAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AlbumAPI/AlbumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AlbumAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
